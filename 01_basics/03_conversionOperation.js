f993d866be0961960bfc311492cecc207d5b4f5b
divyacodehub <mehtadivya776@gmail.com>
2026-08-20 09:06:52 +0000

    dataype_conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = false
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//true => 1, false => 0
+
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" = false
+// "divya" => true
+// undefined => false
+// null => false
+
+let someNumber = 90
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
+
+
+
