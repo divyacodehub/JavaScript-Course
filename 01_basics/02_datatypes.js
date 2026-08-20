664cf366d751fb8c3403833ffed82f29e6805fb8
divyacodehub <mehtadivya776@gmail.com>
2026-08-20 08:36:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js codes as newer version
+
+// alert(3+3) //we are using node.js not browser
+
+let name = "divya";
+let age = 20;
+let ifLoggedIn = true;
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof "divya");
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
+
