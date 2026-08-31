f5f7e0902f0d69a43e1926154f6d16dd4ba39c3d
divyacodehub <mehtadivya776@gmail.com>
2026-08-31 05:58:44 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,41 @@ let stringnumber = String(someNumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
 
+// ******************* operations **********************
+
+let val = 3
+let negval = -val
+console.log(negval);
+
+//console.log(2+2);
+//console.log(2*3);
+
+let str1 = "hello"
+let str2 = " welocome"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 6 + 2);
+console.log(1 + 6 + "2");
+
+let gamecounter = 10
+++gamecounter;
+console.log(gamecounter);
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
