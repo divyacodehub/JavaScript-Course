b02637026371438d5504e5bc6bff948a7dc95c51
divyacodehub <mehtadivya776@gmail.com>
2026-08-31 06:25:17 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(6 > 1);
+// console.log(6 > 1);
+// console.log(6 > 1);
+// console.log(6 > 1);
+// console.log(6 > 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0); //true because comparsion operator(>,<,<=,>=) converts null to number i.e. 0
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+// === strict check - data types too
+
+console.log("2" === 2);
+
+
+
+
