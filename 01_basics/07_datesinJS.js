0332bae1a00d7e891309a2a56876e1f5dec5b7e9
divyacodehub <mehtadivya776@gmail.com>
2026-09-03 05:34:30 +0000

    dates

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,39 @@
+Date
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2026, 0, 24)
+console.log(myCreatedDate.toLocaleString());
+
+// let myCreateDate = new Date(2026, 0, 24,5, 94, 97)
+let myCreateDate = new Date("01-25-2026")
+console.log(myCreateDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());     //time in milliseconds
+
+console.log(Math.floor(Date.now() / 1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+console.log(`${newDate.getDay()} is the day`)
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long"
+}))
+
+
+
