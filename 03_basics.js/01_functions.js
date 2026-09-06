f488ecb9b8d0dfd1a94ed469e09865260ca69f49
divyacodehub <mehtadivya776@gmail.com>
2026-09-06 06:14:54 +0000

    Practice objects and arrays in functions

--- 03_basics.js/01_functions.js
+++ 03_basics.js/01_functions.js
@@ -46,3 +46,45 @@ function loginUserMes (username="khushi"){
      return `${username} just logged in`
 }
 console.log(loginUserMes());
+
+
+// rest operator (... xyz)
+
+function calculatePrice (...numm1){
+    return numm1
+}
+console.log(calculatePrice(200,400,800));
+
+
+function calculateCartPrice (val1, val2, ...numm1){
+    return numm1
+}
+console.log(calculateCartPrice(200,400,300,800));
+
+//objects & function
+
+const user = {
+        username: "divya",
+        price: 2000
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);  
+}
+// handleObject(user)
+
+handleObject({
+    username: "sammy",
+    price: 200
+})
+
+
+//arrays and functions
+
+const newArray = [100, 200, 300]
+
+function returnArrayValue(getArray){
+    return getArray[1]
+}
+// console.log(returnArrayValue(newArray));
+console.log(returnArrayValue([100,2000,300]));
