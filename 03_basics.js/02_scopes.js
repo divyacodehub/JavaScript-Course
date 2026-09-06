c80f759bbf872d91cf46598dab4b2be3313e6a55
divyacodehub <mehtadivya776@gmail.com>
2026-09-06 06:28:55 +0000

    global and local scope

---
+++ 03_basics.js/02_scopes.js
@@ -0,0 +1,14 @@
+//global and local scope
+
+// var c = 200
+let a = 2000
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("inner:", a);
+    
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
