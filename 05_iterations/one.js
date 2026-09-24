c8e186bdb2cfc1b55c74f32fc3ed9079dbe5e54a
divyacodehub <mehtadivya776@gmail.com>
2026-09-24 18:08:58 +0000

    for loop added

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+// for loop
+
+// for(let i = 0; i <= 10; i++){
+//     const elem = i
+//     if(elem == 6)
+//         console.log("yayyyyy");
+//     console.log(elem);
+// }
+
+
+
+
+// for(let i = 1; i <= 5; i++){
+//     for(let j = 1; j <= 5; j++){
+//         console.log(i+j);        
+//     }
+// }
+
+
+
+
+// let heros = ["flash","batman","superman"]
+// console.log(heros);
+
+// for(let i = 0; i < heros.length; i++){
+//     const elem = heros[i]
+//     console.log(elem);
+    
+// }
+
+
+
+
+for(let i = 0; i < 5; i++){
+    if(i == 3){
+        console.log("3 detected");
+    break;
+}
+    console.log(`value of i is ${i}`);
+    
+}
