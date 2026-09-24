f99bda837d8be49b8a22be2ceb65dd6649d6921b
divyacodehub <mehtadivya776@gmail.com>
2026-09-24 18:25:18 +0000

    while loop added

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while loop
+
+
+
+// let index = 0
+//     while(index <= 10){
+//         console.log(`value of i is ${index}`);
+//         index += 2
+//     } 
+
+
+
+// let myArray = ["one","two","three"]
+// let arr  = 0
+//      while(arr < myArray.length){
+//         console.log(myArray[arr]);
+//         arr += 2
+//      }
+
+
+
+let score = 1
+do {
+    console.log(score);
+    score++; 
+}while(score <= 10)
